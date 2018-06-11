4c1451be5731f583fcac542fbca6f85a26150a84
Joachim Ungar <joachim.ungar@eox.at>
2018-06-11 16:25:45 +0200

    overwrite default config parameters if available as environmental variable

--- mapchete_hub/config.py
+++ mapchete_hub/config.py
@@ -1,12 +1,16 @@
+import os
+
+
 def get_host_options():
-    return dict(
+    default = dict(
         host_ip='0.0.0.0',
         port=5000
     )
+    return _get_opts(default)
 
 
 def get_flask_options():
-    return dict(
+    default = dict(
         broker_url='amqp://guest:guest@localhost:5672//',
         result_backend='rpc://guest:guest@localhost:5672//',
         # required to hanlde exceptions raised by billiard
@@ -20,10 +24,13 @@ def get_flask_options():
         task_send_sent_event=True,
         event_queue_expires=604800  # one week in seconds
     )
+    opts = _get_opts(default)
+    print(opts)
+    return opts
 
 
 def get_main_options():
-    return dict(
+    default = dict(
         status_gpkg='status.gpkg',
         status_gpkg_profile=dict(
             crs={'init': 'epsg:4326'},
@@ -46,3 +53,14 @@ def get_main_options():
         ),
         config_dir="/home/ungarj/git/mapchete_hub/tests/testdata/"
     )
+    return _get_opts(default)
+
+
+def _get_opts(default):
+    """
+    Use environmental variables starting with 'MHUB_', otherwise fall back to default
+    values.
+    """
+    return {
+        k: os.environ.get('MHUB_' + k.upper(), default.get(k)) for k in default.keys()
+    }
