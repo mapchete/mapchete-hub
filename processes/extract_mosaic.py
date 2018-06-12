d82f30d8f19b30a09eb6575e8a69d8ca748dee87
Joachim Ungar <joachim.ungar@eox.at>
2018-06-12 14:13:18 +0200

    add timeout option to read()

--- processes/extract_mosaic.py
+++ processes/extract_mosaic.py
@@ -8,19 +8,27 @@ logger = user_process_logger("extract_mosaic")
 
 
 def execute(
-    mp, bands, resampling, mask_clouds, mask_white_areas, read_threads, average_over
+    mp,
+    bands,
+    resampling="nearest",
+    mask_clouds=True,
+    mask_white_areas=True,
+    read_threads=2,
+    timeout=600,
+    average_over=3
 ):
     with mp.open("s2") as src:
         try:
             return cloudless.from_brightness(
                 src.read(
-                    indexes=mp.params["bands"],
-                    resampling=mp.params["resampling"],
-                    mask_clouds=mp.params["mask_clouds"],
-                    mask_white_areas=mp.params["mask_white_areas"],
-                    threads=mp.params["read_threads"]
+                    indexes=bands,
+                    resampling=resampling,
+                    mask_clouds=mask_clouds,
+                    mask_white_areas=mask_white_areas,
+                    threads=read_threads,
+                    timeout=timeout
                 ),
-                average_over=mp.params["average_over"]
+                average_over=average_over
             )
         except EmptyStackException:
             return "empty"
