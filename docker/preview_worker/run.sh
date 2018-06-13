5eee183cb8c683d2b3ed3cf307198bcd7eca1f16
Joachim Ungar <joachim.ungar@eox.at>
2018-06-13 13:32:22 +0200

    update mapfile & add preview worker installation script

---
+++ docker/preview_worker/run.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+CI_JOB_TOKEN=REDACTED_API_KEY
+AWS_ACCESS_KEY_ID='REDACTED_API_KEY'
+AWS_SECRET_ACCESS_KEY='REDACTED_API_KEY'
+MHUB_BROKER_URL='amqp://s2processor:REDACTED_API_KEY@18.197.182.82:5672//'
+MHUB_RESULT_BACKEND='rpc://s2processor:REDACTED_API_KEY@18.197.182.82:5672//'
+MHUB_CONFIG_DIR='/mnt/processes'
+
+# install docker
+curl -fsSL get.docker.com -o get-docker.sh && sudo sh get-docker.sh
+sudo usermod -aG docker ubuntu
+sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
+sudo chmod g+rwx "/home/$USER/.docker" -R
+
+# get rgb_worker docker container
+docker login -u gitlab-ci-token -p $CI_JOB_TOKEN registry.gitlab.eox.at
+
+# make dirs
+sudo mkdir -p /mnt/data/log
+sudo mkdir -p /mnt/data/indexes
+sudo mkdir -p /mnt/data/map/html
+# TODO: copy mapfile into /mnt/data/map and index.html into /mnt/data/map/html
+sudo chown -R ubuntu:ubuntu /mnt/data
+
+# install tools
+sudo apt install -y htop
+
+# launch mapserver container
+docker run \
+  --name mapserver \
+  -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+  -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+  -e MHUB_BROKER_URL=$MHUB_BROKER_URL \
+  -e MHUB_RESULT_BACKEND=$MHUB_RESULT_BACKEND \
+  -e MHUB_CONFIG_DIR=$MHUB_CONFIG_DIR \
+  -e CURL_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt \
+  -e HOST_IP=`curl http://169.254.169.254/latest/meta-data/public-ipv4` \
+  -e LOGLEVEL=$LOGLEVEL \
+  -e LOGFILE=$LOGFILE \
+  -v /mnt/data:/mnt/data \
+  -d \
+  registry.gitlab.eox.at/maps/mapchete_hub/mapserver:latest
+
+
+docker run \
+  --rm \
+  --name=mapserver \
+  -p 80:80 \
+  -v /mnt/data/map/html:/var/www/html \
+  -v /mnt/data/map:/map \
+  -v /mnt/data/indexes:/indexes \
+  -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+  -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+  -d \
+  registry.gitlab.eox.at/maps/mapchete_hub/mapserver
+
+# launch preview_worker container
+docker run \
+  --name preview_worker \
+  -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+  -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+  -e MHUB_BROKER_URL=$MHUB_BROKER_URL \
+  -e MHUB_RESULT_BACKEND=$MHUB_RESULT_BACKEND \
+  -e MHUB_CONFIG_DIR=$MHUB_CONFIG_DIR \
+  -e CURL_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt \
+  -e HOST_IP=`curl http://169.254.169.254/latest/meta-data/public-ipv4` \
+  -e LOGLEVEL=$LOGLEVEL \
+  -e LOGFILE=$LOGFILE \
+  -v /mnt/data:/mnt/data \
+  -d \
+  registry.gitlab.eox.at/maps/mapchete_hub/preview_worker:latest
