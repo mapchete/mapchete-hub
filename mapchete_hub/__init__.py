9c616198444cf657177e3b958d1f4362426965db
Joachim Ungar <joachim.ungar@eox.at>
2018-09-07 11:30:10 +0200

    added AWS requester pays env setting; try to suppress spammy loggers

--- mapchete_hub/__init__.py
+++ mapchete_hub/__init__.py
@@ -1,4 +1,17 @@
+import logging
 from ._core import mapchete_execute, mapchete_index, cleanup_config
 from ._misc import send_to_queue, get_next_jobs, cleanup_datetime
 
 __version__ = "0.1"
+
+
+__all__ = [
+    "mapchete_execute", "mapchete_index", "cleanup_config", "send_to_queue",
+    "get_next_jobs", "cleanup_datetime"
+]
+
+# suppress spam loggers
+logging.getLogger("botocore").setLevel(logging.ERROR)
+logging.getLogger("boto3").setLevel(logging.ERROR)
+logging.getLogger("rasterio").setLevel(logging.ERROR)
+logging.getLogger("smart_open").setLevel(logging.ERROR)
