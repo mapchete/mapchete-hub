f19d933057d876656de15c8e17045dfba116d499
Ubuntu <ubuntu@ip-172-31-26-188.eu-central-1.compute.internal>
2018-10-15 08:25:05 +0000

    use PIL for sharpening

--- mapchete_hub/processes/color_correction.py
+++ mapchete_hub/processes/color_correction.py
@@ -3,19 +3,30 @@
 from mapchete.log import user_process_logger
 from mapchete_s2aws.exceptions import EmptyStackException
 import numpy as np
-from rio_color.operations import sigmoidal, gamma, saturation
+from rasterio.plot import reshape_as_raster, reshape_as_image
+from PIL import Image, ImageFilter
+from rio_color import operations
 
 
 logger = user_process_logger("color_correction")
 
-def execute(mp):
+def execute(
+    mp,
+    bands=[1, 2, 3],
+    red_gamma=1.43,
+    green_gamma=1.3,
+    blue_gamma=1.13,
+    sigmoidal_contrast=8.3,
+    sigmoidal_bias=0.4,
+    saturation=1.3,
+    sharpen_output=False,
+    **kwargs
+):
     """Extract color-corrected, cloud-free image from Sentinel-2 timestack."""
     # (1) read mosaic
     with mp.open("mosaic") as mosaic:
         try:
-            rgb = np.clip(
-                mosaic.read(indexes=mp.params["bands"]) / 16, 0, 255
-            )
+            rgb = np.clip(mosaic.read(indexes=bands) / 16., 0, 255)
         except EmptyStackException:
             return "empty"
 
@@ -24,20 +35,31 @@ def execute(mp):
 
     # (3) color correction using rio-color
     enhanced = np.clip(
-        saturation(         # add saturation
-            sigmoidal(      # add sigmoidal contrast & bias
+        operations.saturation(         # add saturation
+            operations.sigmoidal(      # add sigmoidal contrast & bias
                 np.stack([  # apply gamma correction to each band
-                    gamma(red, mp.params["red_gamma"]),
-                    gamma(green, mp.params["green_gamma"]),
-                    gamma(blue, mp.params["blue_gamma"]),
+                    operations.gamma(red, red_gamma),
+                    operations.gamma(green, green_gamma),
+                    operations.gamma(blue, blue_gamma),
                 ]),
-                mp.params["sigmoidal_contrast"],
-                mp.params["sigmoidal_bias"]
+                sigmoidal_contrast,
+                sigmoidal_bias
             ),
-            mp.params["saturation"]
+            saturation
         ) * 255,    # scale back to 8bit
-        1, 255      # clip valid values to 1 and 255
+        1, 255      # clip valid values to 1 and 255 to avoid nodata
     ).astype("uint8")
+
+    # (5) sharpen image
+    if sharpen_output:
+        logger.debug("sharpen output")
+        enhanced = image_sharpening(enhanced)
 
     # (4) use original nodata mask and return
     return np.where(rgb.mask, mp.params["output"].nodata, enhanced)
+
+
+def image_sharpening(src):
+    return np.clip(reshape_as_raster(
+        Image.fromarray(reshape_as_image(src)).filter(ImageFilter.SHARPEN)
+    ), 1, 255)
