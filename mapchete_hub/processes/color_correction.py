1af7f8dc6dd26c61942a9dfef682fbda250bf67b
Joachim Ungar <joachim.ungar@eox.at>
2018-11-06 10:08:07 +0100

    extended docstring, added geometry clip option

--- mapchete_hub/processes/color_correction.py
+++ mapchete_hub/processes/color_correction.py
@@ -1,5 +1,3 @@
-"""This process extracts cloudless pixels from a time series."""
-
 from mapchete.log import user_process_logger
 from mapchete_s2aws.exceptions import EmptyStackException
 import numpy as np
@@ -33,9 +31,74 @@ def execute(
     des_sigmoidal_bias=0.4,
     des_saturation=1.3,
     sharpen_output=False,
+    clip_buffer=0,
     **kwargs
 ):
-    """Extract color-corrected, cloud-free image from Sentinel-2 mosaic."""
+    """
+    Extract color-corrected, cloud-free image from Sentinel-2 mosaic.
+
+    Inputs:
+    -------
+    mosaic : 3 or 4 band 12bit data
+    clip : vector data used to clip output
+
+    Parameters
+    ----------
+    bands : list
+        List of band indexes.
+    clip_pixelbuffer : int
+        Use pixelbuffer when clipping output by geometry. (default: 0)
+    smooth_water : bool
+        Smooth water areas. (default: True)
+    ndwi_threshold : float
+        Threshold to determine water areas. (default: 0.2)
+    red_gamma : float
+        Gamma correction for red band. (default: 1.43)
+    green_gamma : float
+        Gamma correction for green band. (default: 1.3)
+    blue_gamma : float
+        Gamma correction for blue band. (default: 1.13)
+    sigmoidal_contrast : float
+        Sigmoidal contrast. (default: 8.3)
+    sigmoidal_bias : float
+        Sigmoidal bias. (default: 0.4)
+    saturation : float
+        Saturation. (default: 1.3)
+    cc_desert : bool
+        Use different color correction in desert areas. (default: False)
+    des_ndvi_min : float
+        NDVI minimum threshold to determine desert areas. (default: -0.1)
+    des_ndvi_max : float
+        NDVI maximum threshold to determine desert areas. (default: 0.1)
+    des_ndwi_threshold : float
+        NDWI threshold for desert areas. (default: 0.)
+    des_red_gamma : float
+        Desert area Gamma correction for red band. (default: 1.43)
+    des_green_gamma : float
+        Desert area Gamma correction for green band. (default: 1.3)
+    des_blue_gamma : float
+        Desert area Gamma correction for blue band. (default: 1.13)
+    des_sigmoidal_contrast : float
+        Desert area Sigmoidal contrast. (default: 8.3)
+    des_sigmoidal_bias : float
+        Desert area Sigmoidal bias. (default: 0.4)
+    des_saturation : float
+        Desert area Saturation. (default: 1.3)
+    sharpen_output : bool
+        Apply sharpening filter to output. (default: False)
+
+    Output
+    ------
+    8bit RGB
+    """
+    # read clip geometry
+    if "clip" in mp.params["input"]:
+        clip_geom = mp.open("clip").read()
+        if not clip_geom:
+            logger.debug("no clip data over tile")
+            return "empty"
+    else:
+        clip_geom = []
 
     # read mosaic
     with mp.open("mosaic") as mosaic:
@@ -43,6 +106,7 @@ def execute(
             raw = mosaic.read(indexes=bands).astype(np.int16)
             nodata_mask = raw[0].mask
         except EmptyStackException:
+            logger.debug("no mosaic data over tile")
             return "empty"
 
     if smooth_water:
@@ -75,10 +139,10 @@ def execute(
             rgb = np.where(
                 water_mask,
                 rgb,
-                #image_filters.sharpen(rgb)
-                image_filters.unsharp_mask(
-                    rgb, radius=2, percent=80, threshold=2
-                )
+                image_filters.sharpen(rgb)
+                # image_filters.unsharp_mask(
+                #     rgb, radius=2, percent=80, threshold=2
+                # )
             )
         else:
             rgb = image_filters.sharpen(rgb)
@@ -129,8 +193,12 @@ def execute(
                 corrected
             )
 
-    # use original nodata mask and return
-    return np.where(nodata_mask, mp.params["output"].nodata, corrected)
+    # apply original nodata mask and clip
+    return mp.clip(
+        np.where(nodata_mask, mp.params["output"].nodata, corrected),
+        clip_geom,
+        clip_buffer=clip_buffer
+    )
 
 
 def color_correct(
