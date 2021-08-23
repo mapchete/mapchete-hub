8c02a7c69f1ce021253f3e64900875b30a0b8b36
Petr Ševčík <petr.sevcik@eox.at>
2021-08-23 13:07:00 +0200

    botched arg name

--- mapchete_hub/settings.py
+++ mapchete_hub/settings.py
@@ -37,7 +37,7 @@ WORKER_DEFAULT_SPECS = {
 
 def _get_cluster_specs(gateway, worker_specs):
     options = gateway.cluster_options()
-    options.worker_cores = WORKER_DEFAULT_SPECS[worker_spec]["worker_cores"]
-    options.worker_memory = WORKER_DEFAULT_SPECS[worker_spec]["worker_memory"]
-    options.image = WORKER_DEFAULT_SPECS[worker_spec]["image"]
+    options.worker_cores = WORKER_DEFAULT_SPECS[worker_specs]["worker_cores"]
+    options.worker_memory = WORKER_DEFAULT_SPECS[worker_specs]["worker_memory"]
+    options.image = WORKER_DEFAULT_SPECS[worker_specs]["image"]
     return options
