a397d8e23d38fc5204f32ca1b0ae79788cb6f430
Petr Ševčík <petr.sevcik@eox.at>
2021-10-11 09:29:46 +0200

    set/freeze worker threads settings to 1 as we want to run 1 task per wroker

--- mapchete_hub/settings.py
+++ mapchete_hub/settings.py
@@ -9,6 +9,7 @@ DASK_DEFAULT_SPECS = {
     "default": {
         "worker_cores": 1,
         "worker_memory": 2.0,
+        "worker_threads": 1,
         "scheduler_cores": 1,
         "scheduler_memory": 2.0,
         "image": f"{WORKER_DEFAULT_IMAGE}:{WORKER_DEFAULT_TAG}",
@@ -16,6 +17,7 @@ DASK_DEFAULT_SPECS = {
     "s2_16bit_regular": {
         "worker_cores": 1,
         "worker_memory": 4.0,
+        "worker_threads": 1,
         "scheduler_cores": 1,
         "scheduler_memory": 2.0,
         "image": f"{WORKER_DEFAULT_IMAGE}:{WORKER_DEFAULT_TAG}",
@@ -23,6 +25,7 @@ DASK_DEFAULT_SPECS = {
     "s2_16bit_large": {
         "worker_cores": 1,
         "worker_memory": 8.0,
+        "worker_threads": 1,
         "scheduler_cores": 1,
         "scheduler_memory": 4.0,
         "image": f"{WORKER_DEFAULT_IMAGE}:{WORKER_DEFAULT_TAG}",
@@ -30,6 +33,7 @@ DASK_DEFAULT_SPECS = {
     "s1_large": {
         "worker_cores": 8,
         "worker_memory": 16.0,
+        "worker_threads": 1,
         "scheduler_cores": 1,
         "scheduler_memory": 2.0,
         "image": f"registry.gitlab.eox.at/maps/mapchete_hub/mhub-s1:{WORKER_DEFAULT_TAG}",
@@ -37,6 +41,7 @@ DASK_DEFAULT_SPECS = {
     "custom": {
         "worker_cores": os.environ.get("MHUB_WORKER_CORES", 1),
         "worker_memory": os.environ.get("MHUB_WORKER_MEMORY", 2),
+        "worker_threads": os.environ.get("MHUB_WORKER_THREADS", 1),
         "scheduler_cores": os.environ.get("MHUB_SCHEDULER_CORES", 1),
         "scheduler_memory": os.environ.get("MHUB_SCHEDULER_MEMORY", 2),
         "image": f"{os.environ.get('MHUB_WORKER_IMAGE', WORKER_DEFAULT_IMAGE)}:"
@@ -49,6 +54,8 @@ def _get_cluster_specs(gateway, dask_specs):  # pragma: no cover
     options = gateway.cluster_options()
     options.worker_cores = DASK_DEFAULT_SPECS[dask_specs]["worker_cores"]
     options.worker_memory = DASK_DEFAULT_SPECS[dask_specs]["worker_memory"]
+    # Fix to 1 as we only want to run 1 tasks per worker at a time
+    options.worker_threads = DASK_DEFAULT_SPECS[dask_specs]["worker_threads"]
     options.scheduler_cores = DASK_DEFAULT_SPECS[dask_specs]["scheduler_cores"]
     options.scheduler_memory = DASK_DEFAULT_SPECS[dask_specs]["scheduler_memory"]
     options.image = DASK_DEFAULT_SPECS[dask_specs]["image"]
