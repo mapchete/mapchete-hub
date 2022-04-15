eced5b0df7fb23a79921dbf5178b0390daae4e85
Joachim Ungar <joachim.ungar@eox.at>
2022-04-15 11:00:07 +0200

    also use freshly built container to run pytest

--- run_integration_tests.sh
+++ run_integration_tests.sh
@@ -23,6 +23,7 @@ MHUB_PORT=$(( 5000 + $RANDOM % 1000 ))
 
 if [ "$BUILD" == "TRUE" ]; then
     COMPFILE="docker-compose.yml"
+    TESTFILE="docker-compose.test.yml"
     echo "build image from source"
     docker-compose \
         -p $CI_JOB_ID \
@@ -34,6 +35,7 @@ if [ "$BUILD" == "TRUE" ]; then
         --build-arg EOX_PYPI_TOKEN=${EOX_PYPI_TOKEN} || exit 1
 else
     COMPFILE="docker-compose.image.yml"
+    TESTFILE="docker-compose.image.test.yml"
     echo "build mhub image registry.gitlab.eox.at/maps/mapchete_hub/${IMAGE_NAME:-mhub}:${IMAGE_TAG:-latest}"
     docker build \
         --build-arg BASE_IMAGE_NAME=${BASE_IMAGE_NAME} \
@@ -46,13 +48,13 @@ echo "run mhub on port ${MHUB_PORT}"
 docker-compose \
     -p $CI_JOB_ID \
     -f $COMPFILE \
-    -f docker-compose.test.yml \
+    -f $TESTFILE \
     up \
     --exit-code-from mhub_tester || exit 1
 docker-compose \
     -p $CI_JOB_ID \
     -f $COMPFILE \
-    -f docker-compose.test.yml \
+    -f $TESTFILE \
     down \
     -v \
     --remove-orphans \
