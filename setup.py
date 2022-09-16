2baff81398975ec8237bb30b5ba91fc426d1db9a
Joachim Ungar <joachim.ungar@eox.at>
2022-09-16 10:13:36 +0200

    use latest docker base image; remove deprecated mundi requirements

--- setup.py
+++ setup.py
@@ -63,6 +63,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     setup_requires=["pytest-runner"],
     tests_require=["pytest", "pytest-flask"],
