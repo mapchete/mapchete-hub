46c3dd36dfeda0c32ed23a3d139584d5f1c9b257
Scartography <sevcik.cartography@gmail.com>
2026-01-13 13:26:13 +0100

    try fixing CI/tets for newer cick version

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,7 +4,5 @@ from mapchete_hub.cli.server import main as mhub_server
 
 
 def test_mhub_server():
-    result = CliRunner(env=dict(MAPCHETE_TEST="TRUE"), mix_stderr=True).invoke(
-        mhub_server
-    )
+    result = CliRunner(env=dict(MAPCHETE_TEST="TRUE")).invoke(mhub_server)
     assert result.exit_code == 0
